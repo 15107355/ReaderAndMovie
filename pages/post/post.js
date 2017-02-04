4132860a30a8eb9b72911d6e1c35910267452a23
15107355 <15107355@qq.com>
2017-02-04 23:01:01 +0800

    2017.2.4 23:00 用父级获取catchTap的target id元素，实现代码的简化

--- pages/post/post.js
+++ pages/post/post.js
@@ -9,34 +9,52 @@ Page({
     });
   },
 
-  onPostTap:function(event){
+  onPostTap: function (event) {
     var postId = event.currentTarget.dataset.postid;
     // console.log("on Post id is" + postId);
     wx.navigateTo({
       url: 'post-detail/post-detail?id=' + postId,
-      success: function(res){
+      success: function (res) {
         // success
       },
-      fail: function() {
+      fail: function () {
         // fail
       },
-      complete: function() {
+      complete: function () {
         // complete
       }
     })
   },
-  onSwiperItemTap:function(event){
-    var postId = event.currentTarget.dataset.postid;
+  // onSwiperItemTap:function(event){
+  //   var postId = event.currentTarget.dataset.postid;
+  //   // console.log("on Post id is" + postId);
+  //   wx.navigateTo({
+  //     url: 'post-detail/post-detail?id=' + postId,
+  //     success: function(res){
+  //       // success
+  //     },
+  //     fail: function() {
+  //       // fail
+  //     },
+  //     complete: function() {
+  //       // complete
+  //     }
+  //   })
+  onSwiperTap: function (event) {
+    // target 和currentTarget
+    // target指的是当前点击的组件 和currentTarget 指的是事件捕获的组件
+    // target这里指的是image，而currentTarget指的是swiper
+    var postId = event.target.dataset.postid;
     // console.log("on Post id is" + postId);
     wx.navigateTo({
       url: 'post-detail/post-detail?id=' + postId,
-      success: function(res){
+      success: function (res) {
         // success
       },
-      fail: function() {
+      fail: function () {
         // fail
       },
-      complete: function() {
+      complete: function () {
         // complete
       }
     })
