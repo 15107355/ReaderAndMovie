66855f5804021a6a43cdc759378656d3ccc7c6e2
15107355 <15107355@qq.com>
2017-02-05 19:50:17 +0800

    2017.2.5 19:49 设置request合法域名以及配置豆瓣top250 api

--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -3,6 +3,23 @@ Page({
   data:{},
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
+    wx.request({
+      url: 'https://api.douban.com/v2/movie/top250',
+      data: {},
+      method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      header: {
+        "Content-Type":"json"
+      }, // 设置请求的 header
+      success: function(res){
+        console.log(res)
+      },
+      fail: function(error) {
+        console.log(error)
+      },
+      complete: function() {
+        // complete
+      }
+    })
   },
   onReady:function(){
     // 页面渲染完成
