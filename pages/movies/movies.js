463f7b7bb8815a9050ae76c9c51d3d670a7e0a2c
15107355 <15107355@qq.com>
2017-02-05 22:34:13 +0800

    2017.2.5 22:30 电影页调用成功，需要重新学习7-11至7-16

--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -1,17 +1,22 @@
 var app = getApp();
 Page({
-  data: {},
+  data: {
+    inTheaters: {},
+    comingSoon: {},
+    top250: {},
+  },
   onLoad: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
     var inTheatersUrl = app.globalData.doubanBase + "/v2/movie/in_theaters" + "?start=0&count=3";
     var comingSoonUrl = app.globalData.doubanBase + "/v2/movie/coming_soon" + "?start=0&count=3";
     var top250Url = app.globalData.doubanBase + "/v2/movie/top250" + "?start=0&count=3";
 
-    this.getMovieListData(inTheatersUrl);
-    // this.getMovieListData(comingSoonUrl);
-    // this.getMovieListData(top250Url);
+    this.getMovieListData(inTheatersUrl, "inTheaters");
+    this.getMovieListData(comingSoonUrl, "comingSoon");
+    this.getMovieListData(top250Url, "top250");
+    // 以上inTheaters，comingSoon，top250三个KEY值是对应上面data里面的的3个数据
   },
-  getMovieListData: function (doubanUrl) {
+  getMovieListData: function (doubanUrl, settedKey) {
     var that = this;
     wx.request({
       url: doubanUrl,
@@ -22,7 +27,7 @@ Page({
       }, // 设置请求的 header
       success: function (res) {
         console.log(res);
-        that.processDoubanData(res.data);
+        that.processDoubanData(res.data,settedKey);
       },
       fail: function (error) {
         console.log(error)
@@ -32,7 +37,7 @@ Page({
       }
     })
   },
-  processDoubanData: function (moviesDouban) {
+  processDoubanData: function (moviesDouban, settedKey) {
     var movies = [];
     for (var idx in moviesDouban.subjects) {
       var subject = moviesDouban.subjects[idx];
@@ -49,9 +54,14 @@ Page({
       movies.push(tempData)
 
     }
-    this.setData({
-      dMovies: movies
-    })
+    var readyData = {};
+    // 1.定义readyData为空
+    readyData[settedKey] = {
+      dmovies:movies
+      };
+    // 2.readyData的数组settedKey为movies的数组：每个movies下面都有一个dmovies的属性值
+    this.setData(readyData);
+    // 3. 为readyData赋值
 
   },
   onReady: function () {
