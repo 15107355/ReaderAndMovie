d22b5690b03e8da2f43aeedc6bc90cb21966608d
15107355 <15107355@qq.com>
2017-02-05 21:26:32 +0800

    2017.2.5 21:26 调用豆瓣近期上映数据成功

--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -1,36 +1,69 @@
-// pages/movies/movies.js
+var app = getApp();
 Page({
-  data:{},
-  onLoad:function(options){
+  data: {},
+  onLoad: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
+    var inTheatersUrl = app.globalData.doubanBase + "/v2/movie/in_theaters" + "?start=0&count=3";
+    var comingSoonUrl = app.globalData.doubanBase + "/v2/movie/coming_soon" + "?start=0&count=3";
+    var top250Url = app.globalData.doubanBase + "/v2/movie/top250" + "?start=0&count=3";
+
+    this.getMovieListData(inTheatersUrl);
+    // this.getMovieListData(comingSoonUrl);
+    // this.getMovieListData(top250Url);
+  },
+  getMovieListData: function (doubanUrl) {
+    var that = this;
     wx.request({
-      url: 'https://api.douban.com/v2/movie/top250',
+      url: doubanUrl,
       data: {},
       method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
       header: {
-        "Content-Type":"json"
+        "Content-Type": "json"
       }, // 设置请求的 header
-      success: function(res){
-        console.log(res)
+      success: function (res) {
+        console.log(res);
+        that.processDoubanData(res.data);
       },
-      fail: function(error) {
+      fail: function (error) {
         console.log(error)
       },
-      complete: function() {
+      complete: function () {
         // complete
       }
     })
   },
-  onReady:function(){
+  processDoubanData: function (moviesDouban) {
+    var movies = [];
+    for (var idx in moviesDouban.subjects) {
+      var subject = moviesDouban.subjects[idx];
+      var title = subject.title;
+      if (title.length >= 6) {
+        title = title.substring(0, 6) + "...";
+      }
+      var tempData = {
+        title: title,
+        average: subject.rating.average,
+        coverageUrl: subject.images.large,
+        movieId: subject.id,
+      }
+      movies.push(tempData)
+
+    }
+    this.setData({
+      dMovies: movies
+    })
+
+  },
+  onReady: function () {
     // 页面渲染完成
   },
-  onShow:function(){
+  onShow: function () {
     // 页面显示
   },
-  onHide:function(){
+  onHide: function () {
     // 页面隐藏
   },
-  onUnload:function(){
+  onUnload: function () {
     // 页面关闭
   }
 })
